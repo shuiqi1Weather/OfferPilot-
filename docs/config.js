8b950fca0f8c0a514ff2d228d8b6706333b3485c
shuiqi1Weather <2959905073@qq.com>
2026-09-22 21:57:00 +0800

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,6 @@
+// GitHub Pages 部署时自动识别仓库。使用自定义域名时，可填 owner/repository。
+window.OFFERPILOT_CONFIG = Object.freeze({
+  repository: "",
+  assetName: "OfferPilot_v53_Windows.zip",
+  version: "v53"
+});
