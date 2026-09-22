7569e38ad00bae33eadf06a6b11f4b449e470b17
shuiqi1Weather <2959905073@qq.com>
2026-09-22 22:13:24 +0800

    Delete OfferPilot 航途 directory

--- OfferPilot 航途/docs/config.js
+++
@@ -1,6 +0,0 @@
-// GitHub Pages 部署时自动识别仓库。使用自定义域名时，可填 owner/repository。
-window.OFFERPILOT_CONFIG = Object.freeze({
-  repository: "",
-  assetName: "OfferPilot_v53_Windows.zip",
-  version: "v53"
-});
